ad74999ed1d2961469be6c0ec6c3ef94247d1799
Malhar Devasthali <32365799+mallya007@users.noreply.github.com>
2019-04-26 11:16:11 +0530

    Create Bird.cs

---
+++ Bird.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    public float upForce=200f;
+    private Animator anim;
+    private GameControl gc;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        gc = GameObject.Find("GameController").GetComponent<GameControl>();
+    }
+
+
+    void Update()
+    {
+        if(isDead==false)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                anim.SetTrigger("Flap");
+               rb2d.velocity = Vector2.zero; //for nullifing the effect of prev. flap everytime
+                rb2d.AddForce(new Vector2(0,upForce));
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb2d.velocity = Vector2.zero;
+        isDead = true;
+        anim.SetTrigger("Die");
+        gc.BirdDied();
+    }
+
+}
